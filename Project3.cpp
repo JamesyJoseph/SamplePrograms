ebb660f8ef8ded10947dca774972ec27aa11c0f7
JamesyJoseph <118750190+JamesyJoseph@users.noreply.github.com>
2024-12-20 03:53:18 +0530

    Create Project3.cpp

---
+++ Project3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int n,nf=1;
+cout<<"Enter the number:";
+cin>>n;
+for(int i=1;i<=n;i++)
+{
+nf=nf*i;
+}
+cout<<"Factorial :" <<nf;
+}
